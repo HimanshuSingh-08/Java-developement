a10619ecee37b5b53de3d5c91ee0413ca1f69a74
Himanshu Singh <himansh.singh293@gmail.com>
2025-04-13 08:57:27 +0530

    covered basic data types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        System.out.print("Hello this is my first java code");
+
+
+        // Data Types 
+        int num_int = 1;
+        double num_double = 1234567.343;
+        float num_float = 23903209;
+        char letter_char = 'H';
+        String name_string ="Himanshu Singh";
+        System.out.println(num_int);
+        System.out.println(num_double);
+        System.out.println(num_float);
+        System.out.println(letter_char);
+        System.out.println(name_string);
+
+   }
 }
